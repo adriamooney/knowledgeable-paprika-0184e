2f1b49094abc2541143f1fbee7ce02b1b403c522
Stackbit Code Editor <projects@stackbit.com>
2021-10-13 20:48:03 +0000

    ContentSection.js: changed

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 
 import { markdownify } from '../utils';
-import {html}
+import {htmlTo}
 
 export default class ContentSection extends React.Component {
     render() {
