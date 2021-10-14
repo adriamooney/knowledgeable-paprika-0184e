13a05ecac25fec498abe8fb1d219fd09db72cb46
Stackbit Code Editor <projects@stackbit.com>
2021-10-14 19:03:53 +0000

    ContentSection.js: changed

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -10,7 +10,7 @@ export default class ContentSection extends React.Component {
         const content = _.get(section, 'content');
 
         return (
-            <section className="section">
+            <section className="section content-section">
                 <div className="container container--md">
                     {title && <h2 className="section__title align-center">{title}</h2>}
                     {content && <div className="section__copy">{markdownify(content)}</div>}
